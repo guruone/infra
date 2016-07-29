4cf71c5049c5b8c7db2f26eb271d2beaca4a171a
vauxhall <zero.griffin@gmail.com>
2016-07-29 17:21:00 -0500

    added pods for firebase

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'infra' do
+  pod 'Firebase/Core'
+  pod 'Firebase/Storage'
+end
---
+++ Pods/Target Support Files/Pods-infra/Pods-infra-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_infra : NSObject
+@end
+@implementation PodsDummy_Pods_infra
+@end
