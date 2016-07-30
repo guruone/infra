708f3cf83534dc893d9609ca04aaed92161f7cc0
vauxhall <zero.griffin@gmail.com>
2016-07-29 21:29:16 -0500

    firebase storage

--- infra/Controller/Firebase/CFirebaseStorage.swift
+++ infra/Controller/Firebase/CFirebaseStorage.swift
@@ -1,6 +1,25 @@
 import Foundation
+import Firebase
 
 class CFirebaseStorage
 {
+    private let storage:FIRStorage
+    private let storageReference:FIRStorageReference
+    private let kStorageReference:String = "gs://infra-3cd0d.appspot.com"
+    private let kStorageReference_News:String = "news"
     
+    init()
+    {
+        storage = FIRStorage.storage()
+        storageReference = storage.reference()
+    }
+    
+    //MARK: public
+    
+    func news() -> FIRStorageReference
+    {
+        let reference:FIRStorageReference = storageReference.child(kStorageReference_News)
+        
+        return reference
+    }
 }
--- infra/Controller/Landing/CLanding.swift
+++ infra/Controller/Landing/CLanding.swift
@@ -9,5 +9,26 @@ class CLanding:CMainController
         let viewLanding:VLanding = VLanding(controller:self)
         self.viewLanding = viewLanding
         view = viewLanding
+        
+        CFirebase.sharedInstance.storage.news().metadataWithCompletion
+        { (metadata, error) in
+            
+            if error != nil
+            {
+                print("error \(error!.localizedDescription)")
+            }
+            else
+            {
+                let urls:[NSURL]? = metadata?.downloadURLs
+                
+                if urls != nil
+                {
+                    for url in urls!
+                    {
+                        print(url)
+                    }
+                }
+            }
+        }
     }
 }
