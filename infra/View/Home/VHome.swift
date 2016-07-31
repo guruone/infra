07c6c51078c754988f646382b3e2edb73a41644d
vauxhall <zero.griffin@gmail.com>
2016-07-31 17:13:52 -0500

    added loader

--- infra/View/Home/VHome.swift
+++ infra/View/Home/VHome.swift
@@ -3,10 +3,15 @@ import UIKit
 class VHome:UIView
 {
     weak var controller:CHome!
+    weak var spinner:VMainLoader!
     
     convenience init(controller:CHome)
     {
         self.init()
         self.controller = controller
+        
+        let spinner:VMainLoader = VMainLoader()
+        
+        addSubview(spinner)
     }
 }
