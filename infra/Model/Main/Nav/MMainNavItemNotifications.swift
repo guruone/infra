a4870a396a0e056e5d671f2922d98ceb12afbebf
vauxhall <zero.griffin@gmail.com>
2016-08-02 21:14:19 -0500

    item notifications

---
+++ infra/Model/Main/Nav/MMainNavItemNotifications.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+class MMainNavItemNotifications:MMainNavItem
+{
+    private let kImage:String = "navigationHome"
+    
+    init()
+    {
+        super.init(image:kImage)
+    }
+}
