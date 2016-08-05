e936ab1d41eb288c3467bd5ba9da46543e5d8a2a
vauxhall <zero.griffin@gmail.com>
2016-08-05 15:32:59 -0500

    constraints

--- infra/View/Onboarding/VOnboardingItemHistory.swift
+++ infra/View/Onboarding/VOnboardingItemHistory.swift
@@ -3,6 +3,10 @@ import UIKit
 class VOnboardingItemHistory:UIView
 {
     weak var controller:COnboardingItemHistory!
+    weak var layoutImageLeft:NSLayoutConstraint!
+    private let kImageWidth:CGFloat = 180
+    private let kImageHeight:CGFloat = 214
+    private let kImageTop:CGFloat = 30
     
     convenience init(controller:COnboardingItemHistory)
     {
@@ -14,7 +18,7 @@ class VOnboardingItemHistory:UIView
         let image:UIImageView = UIImageView()
         image.userInteractionEnabled = false
         image.clipsToBounds = true
-        image.contentMode = UIViewContentMode.Bottom
+        image.contentMode = UIViewContentMode.ScaleAspectFit
         image.translatesAutoresizingMaskIntoConstraints = false
         image.image = UIImage(named:"onboardingHistory")
         
@@ -23,17 +27,31 @@ class VOnboardingItemHistory:UIView
         let views:[String:AnyObject] = [
             "image":image]
         
-        let metrics:[String:AnyObject] = [:]
+        let metrics:[String:AnyObject] = [
+            "imageTop":kImageTop,
+            "imageWidth":kImageWidth,
+            "imageHeight":kImageHeight]
         
         addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:|-0-[image]-0-|",
+            "H:[image(imageWidth)]",
             options:[],
             metrics:metrics,
             views:views))
         addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:|-0-[image(100)]",
+            "V:|-(imageTop)-[image(imageHeight)]",
             options:[],
             metrics:metrics,
             views:views))
+        
+        layoutImageLeft = NSLayoutConstraint(
+            item:image,
+            attribute:NSLayoutAttribute.Left,
+            relatedBy:NSLayoutRelation.Equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.Left,
+            multiplier:1,
+            constant:0)
+        
+        addConstraint(layoutImageLeft)
     }
 }
