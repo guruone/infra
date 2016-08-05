99296edc5fa1da266ba45094a24df98321069036
vauxhall <zero.griffin@gmail.com>
2016-08-05 12:32:01 -0500

    f model poem

--- infra/Firebase/Database/Models/FDatabaseModelPoem.swift
+++ infra/Firebase/Database/Models/FDatabaseModelPoem.swift
@@ -2,43 +2,19 @@ import Foundation
 
 class FDatabaseModelPoem:FDatabaseModel
 {
-    var access:DInfraUser.DInfraUserAccess
     private let kKeyTitle:String = "title"
     private let kKeyUserId:String = "user_id"
     private let kKeyStatus:String = "status"
     private let kKeyCreated:String = "created"
     private let kKeyLastEdit:String = "last_edit"
-    private let kKeyLikes:String
-
-    init(dbUser:DInfraUser)
-    {
-        access = dbUser.access
-    }
-    
-    init(snapshot:AnyObject?)
-    {
-        let json:[String:AnyObject]? = snapshot as? [String:AnyObject]
-        
-        if json != nil
-        {
-            let rawAccess:Int = json![kKeyAccess] as! Int
-            let rawAccessPrimitive:Int16 = Int16(rawAccess)
-            access = DInfraUser.DInfraUserAccess(rawValue:rawAccessPrimitive)!
-        }
-        else
-        {
-            access = DInfraUser.DInfraUserAccess.Banned
-        }
-    }
+    private let kKeyLikes:String = "likes"
     
     //MARK: public
     
     func modelJson() -> [String:AnyObject]
     {
-        let accessNumber:Int = Int(access.rawValue)
-        
         let dict:[String:AnyObject] = [
-            kKeyAccess:accessNumber
+            :
         ]
         
         return dict
