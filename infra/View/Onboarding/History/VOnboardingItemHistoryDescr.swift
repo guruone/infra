d63469b6cef08f1a429c09b028aa3b1c79e31201
vauxhall <zero.griffin@gmail.com>
2016-08-05 20:07:35 -0500

    create description cell for onboarding history

--- infra/View/Onboarding/History/VOnboardingItemHistoryDescr.swift
+++ infra/View/Onboarding/History/VOnboardingItemHistoryDescr.swift
@@ -2,5 +2,42 @@ import UIKit
 
 class VOnboardingItemHistoryDescr:UICollectionViewCell
 {
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clearColor()
+        
+        let label:UILabel = UILabel()
+        label.userInteractionEnabled = false
+        label.backgroundColor = UIColor.clearColor()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.regular(15)
+        label.textColor = UIColor.main()
+        label.numberOfLines = 0
+        label.text = NSLocalizedString("VOnboardingItemHistoryDescr_label", comment:"")
+        
+        addSubview(label)
+        
+        let views:[String:AnyObject] = [
+            "label":label]
+        
+        let metrics:[String:AnyObject] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-10-[label]-10-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-20-[label]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+    }
     
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
 }
