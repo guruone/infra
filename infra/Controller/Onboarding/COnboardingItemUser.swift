4bfbc51616566ac7d41d4e0bf2638cec10aa5206
vauxhall <zero.griffin@gmail.com>
2016-08-06 16:29:00 -0500

    default name

--- infra/Controller/Onboarding/COnboardingItemUser.swift
+++ infra/Controller/Onboarding/COnboardingItemUser.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class COnboardingItemUser:COnboardingItem, UITextFieldDelegate
 {
+    private var name:String?
+    
     override func loadView()
     {
         view = VOnboardingItemUser(controller:self)
@@ -11,10 +13,22 @@ class COnboardingItemUser:COnboardingItem, UITextFieldDelegate
     
     func actionStart(sender button:UIButton)
     {
-        
+        if name == nil
+        {
+            name = NSLocalizedString("COnboardingItemUser_defaultName", comment:"")
+        }
+        else if name!.isEmpty
+        {
+            name = NSLocalizedString("COnboardingItemUser_defaultName", comment:"")
+        }
     }
     
     //MARK: field del
+    
+    func textFieldDidEndEditing(textField:UITextField)
+    {
+        name = textField.text
+    }
     
     func textFieldShouldReturn(textField:UITextField) -> Bool
     {
