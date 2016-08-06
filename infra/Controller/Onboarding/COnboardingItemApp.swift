addca65cf7973cdf9d0fe07d772d0964eea0aa37
vauxhall <zero.griffin@gmail.com>
2016-08-05 22:06:16 -0500

    image on onboarding app

--- infra/Controller/Onboarding/COnboardingItemApp.swift
+++ infra/Controller/Onboarding/COnboardingItemApp.swift
@@ -2,5 +2,8 @@ import UIKit
 
 class COnboardingItemApp:COnboardingItem
 {
-    
+    override func loadView()
+    {
+        view = VOnboardingItemApp(controller:self)
+    }
 }
--- infra/View/Onboarding/App/VOnboardingItemApp.swift
+++ infra/View/Onboarding/App/VOnboardingItemApp.swift
@@ -3,6 +3,7 @@ import UIKit
 class VOnboardingItemApp:UIView
 {
     weak var controller:COnboardingItemApp!
+    private let kImageHeight:CGFloat = 120
     
     convenience init(controller:COnboardingItemApp)
     {
@@ -12,6 +13,30 @@ class VOnboardingItemApp:UIView
         userInteractionEnabled = false
         self.controller = controller
         
+        let image:UIImageView = UIImageView()
+        image.userInteractionEnabled = false
+        image.clipsToBounds = true
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = UIViewContentMode.Center
+        image.image = UIImage(named:"onboardingApp")
         
+        addSubview(image)
+        
+        let views:[String:AnyObject] = [
+            "image":image]
+        
+        let metrics:[String:AnyObject] = [
+            "imageHeight":kImageHeight]
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-0-[image]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-20-[image(imageHeight)]",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
 }
