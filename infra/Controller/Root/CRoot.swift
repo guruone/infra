1ccb1ef4709d5908936fb0407e8c7a0fb5212a4f
vauxhall <zero.griffin@gmail.com>
2016-08-08 09:43:18 -0500

    model root poems

--- infra/Controller/Root/CRoot.swift
+++ infra/Controller/Root/CRoot.swift
@@ -4,6 +4,11 @@ class CRoot:CMainController
 {
     weak var viewRoot:VRoot!
     
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
     override func loadView()
     {
         let viewRoot:VRoot = VRoot(controller:self)
