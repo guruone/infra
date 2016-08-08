4f0c691b88b2f38e01fa7e294733d11a8d3329d5
vauxhall <zero.griffin@gmail.com>
2016-08-08 10:04:21 -0500

    model poems lists and state

---
+++ infra/Model/Root/Poems/MRootPoemsList.swift
@@ -0,0 +1,9 @@
+//
+//  MRootPoemsItem.swift
+//  infra
+//
+//  Created by zero on 8/8/16.
+//  Copyright © 2016 iturbide. All rights reserved.
+//
+
+import Foundation
---
+++ infra/Model/Root/Poems/MRootPoemsStateReview.swift
@@ -0,0 +1,9 @@
+//
+//  MRootPoemsStateReview.swift
+//  infra
+//
+//  Created by zero on 8/8/16.
+//  Copyright © 2016 iturbide. All rights reserved.
+//
+
+import Foundation
