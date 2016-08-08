9381fb7fc63b0625b3a1380988a2476cedbb0e3f
vauxhall <zero.griffin@gmail.com>
2016-08-08 09:31:46 -0500

    controller root implements view root

--- infra/Controller/Root/CRoot.swift
+++ infra/Controller/Root/CRoot.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class CRoot:CMainController
 {
+    weak var viewRoot:VRoot!
     
+    override func loadView()
+    {
+        let viewRoot:VRoot = VRoot(controller:self)
+        self.viewRoot = viewRoot
+        view = viewRoot
+    }
 }
--- infra/View/Root/VRoot.swift
+++ infra/View/Root/VRoot.swift
@@ -2,5 +2,14 @@ import UIKit
 
 class VRoot:UIView
 {
+    weak var controller:CRoot!
     
+    convenience init(controller:CRoot)
+    {
+        self.init()
+        clipsToBounds = true
+        backgroundColor = UIColor.whiteColor()
+        translatesAutoresizingMaskIntoConstraints = false
+        self.controller = controller
+    }
 }
