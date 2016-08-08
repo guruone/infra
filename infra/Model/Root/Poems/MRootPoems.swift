5c4b0ce1fcf9ce1993adaea4edadd48de54fb9e5
vauxhall <zero.griffin@gmail.com>
2016-08-08 09:54:23 -0500

    model poems listens firebase

--- infra/Model/Root/Poems/MRootPoems.swift
+++ infra/Model/Root/Poems/MRootPoems.swift
@@ -1,6 +1,17 @@
 import Foundation
+import FirebaseDatabase
 
 class MRootPoems
 {
+    init()
+    {
+        FMain.sharedInstance.database.listenPoems(snapBlock)
+    }
     
+    //MARK: private
+    
+    private func snapBlock(snapshot:FIRDataSnapshot)
+    {
+        
+    }
 }
