b9639efa18ffa04aff4c627ee34c9ebe3d900f7e
vauxhall <zero.griffin@gmail.com>
2016-08-09 21:25:56 -0500

    factory actions

--- infra/Model/Root/Poems/Actions/MRootPoemsListItemAction.swift
+++ infra/Model/Root/Poems/Actions/MRootPoemsListItemAction.swift
@@ -2,4 +2,24 @@ import Foundation
 
 class MRootPoemsListItemAction
 {
+    class func Accept() -> MRootPoemsListItemActionAccept
+    {
+        let action:MRootPoemsListItemActionAccept = MRootPoemsListItemActionAccept()
+        
+        return action
+    }
+    
+    class func Reject() -> MRootPoemsListItemActionReject
+    {
+        let action:MRootPoemsListItemActionReject = MRootPoemsListItemActionReject()
+        
+        return action
+    }
+    
+    class func Revision() -> MRootPoemsListItemActionRevision
+    {
+        let action:MRootPoemsListItemActionRevision = MRootPoemsListItemActionRevision()
+        
+        return action
+    }
 }
